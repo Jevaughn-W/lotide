dffbeb5c7064e7df2b1fae3c6945e000fdf63b4d
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-15 15:28:52 -0400

    AssertArraysEqual that prints to console on its own

---
+++ assertArraysEqual.js
@@ -0,0 +1,40 @@
+/*const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+*/
+// take in the arrays 1 and arrays 2
+const eqArrays = function(firstArray, secondArray) {
+  // testing that each length of the arrays are the same first, if not return false
+  if (firstArray.length === secondArray.length) {
+    // loop through each element of the arrays simultaneously
+    for (let i = 0; i < firstArray.length; i++) {
+      // test if each element is equal to each other
+      if (firstArray[i] !== secondArray [i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// first take in the arrays as parameters
+const assertArraysEquals = function(arrayOne, arrayTwo) {
+  // then insert them into the eq arrays function
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`😓 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+assertArraysEquals([1,2,3,4],[1,2,3,4]);
+assertArraysEquals([1,2,3,"4"],[1,2,3,4]);
+assertArraysEquals([1,2,3,5],[1,2,3,4]);
+assertArraysEquals(["a","d",3,5],[1,2,3,4]);

78fb729fd2df692477286147947c2d678ae7b392
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-16 13:24:00 -0400

    corrected formatting

--- without.js
+++ without.js
@@ -33,9 +33,9 @@ const without = function(source, itemsToRemove) {
   // working to alter the new array
   for (let j = 0; j < cleansedArray.length; j++) {
     for (let i = 0; i < itemsToRemove.length; i++) {
-      if (cleansedArray[j] === itemsToRemove[i]){
+      if (cleansedArray[j] === itemsToRemove[i]) {
         cleansedArray.splice(j,1);
-      }   
+      }
     }
   }
   return cleansedArray;

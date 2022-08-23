59506d2d42c3105926ff0962c48fb487fe9cd175
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-23 18:45:17 -0400

    Function which finds the Object key of a value in an inner object

---
+++ findKey.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else {
+    console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// 1. Use a for in function to iterate through the higher level of objects
+// 2. Use box notation to push the inner object to the callback
+// 3. return the key if found otherwise return undefined
+
+const findKey = function (object, callback) {
+  for(let item in object) {
+    if(callback(object[item])){
+      return item;
+      
+    }
+    
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 4 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 6 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), undefined);
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars % 3 === 0), "Akaleri");

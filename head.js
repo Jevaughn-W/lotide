5e37db0b1fbfe99cae0bf00da060ff67bc02277b
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 18:38:23 -0400

    Implemented reqiure to impout assert equal and moved test code

--- head.js
+++ head.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual.js');
 
 const head = function(sampleArray) {
   return sampleArray[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "labs"]), "Hello");
-assertEqual(head([8,7,6]), 8);
-assertEqual(head([7,6,77]), 7);
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
+module.exports = head;

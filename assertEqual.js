a364e1ead26397ca714bf09c38058fed2ea37876
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 18:37:42 -0400

    Implemented module export

--- assertEqual.js
+++ assertEqual.js
@@ -7,11 +7,4 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("this_works","this_works");
-assertEqual("jevaughn","williams");
-assertEqual(200,12);
-assertEqual(42,42);
-assertEqual("Jevaughn",'jevaughn');
+module.exports = assertEqual;

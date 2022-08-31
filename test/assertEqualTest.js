7f57c8d4b4d3aa07dad9b3d27bfeefad362091a8
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 18:39:28 -0400

    move assertEqual tests here

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("this_works","this_works");
+assertEqual("jevaughn","williams");
+assertEqual(200,12);
+assertEqual(42,42);
+assertEqual("Jevaughn",'jevaughn');

a1df43343a8fa3210324bcb5bdc8108cab06a738
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 20:54:50 -0400

    refactor to import and export necessary function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,32 +1,15 @@
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
-*/
-// take in the arrays 1 and arrays 2
-const eqArrays = function(firstArray, secondArray) {
-  // testing that each length of the arrays are the same first, if not return false
-  if (firstArray.length === secondArray.length) {
-    // loop through each element of the arrays simultaneously
-    for (let i = 0; i < firstArray.length; i++) {
-      // test if each element is equal to each other
-      if (firstArray[i] !== secondArray [i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-};
+// About Code:
 
 // first take in the arrays as parameters
+// then insert them into the eq arrays function
+
+//Importing eqArrays
+
+const eqArrays = require('./eqArrays.js');
+
+// Driver Code
+
 const assertArraysEqual = function(arrayOne, arrayTwo) {
-  // then insert them into the eq arrays function
   if (eqArrays(arrayOne, arrayTwo)) {
     console.log(`✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
   } else {
@@ -34,7 +17,6 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
   }
 };
 
-assertArraysEqual([1,2,3,4],[1,2,3,4]);
-assertArraysEqual([1,2,3,"4"],[1,2,3,4]);
-assertArraysEqual([1,2,3,5],[1,2,3,4]);
-assertArraysEqual(["a","d",3,5],[1,2,3,4]);
+// Export asserArrays Equal
+
+module.exports = assertArraysEqual;

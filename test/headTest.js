8c8bb60693d78c7dc9248beed49a070d49c8f0ea
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 21:58:18 -0400

    implemented mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,37 @@
+const assert = require('chai').assert;
 const head = require('../head.js');
-const assertEqual = require('../assertEqual');
 
-// Test Code
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "labs"]), "Hello");
-assertEqual(head([8,7,6]), 8);
-assertEqual(head([7,6,77]), 7);
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
+//Test Code
+
+describe("function which returns the first element of an array", () => {
+  it("should return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("should return 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("should return 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("should return hello for ['Hello', 'Lighthouse', 'labs']), 'Hello']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "labs"]), "Hello");
+  });
+
+  it("should return 8 for [8, 7, 6]", () => {
+    assert.strictEqual(head([8, 7 , 6]), 8);
+  });
+
+  it("should return 7 for [7, 6, 77]", () => {
+    assert.strictEqual(head([7, 6, 77]), 7);
+  });
+  it("should return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("should return 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+});

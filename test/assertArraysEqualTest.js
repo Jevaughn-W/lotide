671017d0bab503a8ad16ea674316e7b691897f19
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 20:56:38 -0400

    Move over asserArraysEqual test code here

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+// Importing function
+
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+assertArraysEqual([1,2,3,4],[1,2,3,4]); // pass
+assertArraysEqual([1,2,3,"4"],[1,2,3,4]); // fail
+assertArraysEqual([1,2,3,5],[1,2,3,4]); // fail
+assertArraysEqual(["a","d",3,5],[1,2,3,4]); // fail

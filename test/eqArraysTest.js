54e9420c3f5991625eecd4b94394a350c2c72a00
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 20:46:22 -0400

    eqArrays Test moved test code here

---
+++ test/eqArraysTest.js
@@ -0,0 +1,12 @@
+// Importing required functions
+
+const assertEqual = require('../assertEqual.js');
+const eqArrays = require('../eqArrays.js');
+
+// Test Code
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false

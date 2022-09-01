c63859e4aaf0c04faa59e8095096601fe1fce597
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-08-31 20:45:29 -0400

    refactored to export function

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
+//About code:
 // take in the arrays 1 and arrays 2
+// testing that each length of the arrays are the same first, if not return false
+// loop through each element of the arrays simultaneously
+// test if each element is equal to each other
+
+
+//Driver Code
 const eqArrays = function(firstArray, secondArray) {
-  // testing that each length of the arrays are the same first, if not return false
   if (firstArray.length === secondArray.length) {
-    // loop through each element of the arrays simultaneously
     for (let i = 0; i < firstArray.length; i++) {
-      // test if each element is equal to each other
       if (firstArray[i] !== secondArray [i]) {
         return false;
       }
@@ -24,8 +19,6 @@ const eqArrays = function(firstArray, secondArray) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+//Export
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;

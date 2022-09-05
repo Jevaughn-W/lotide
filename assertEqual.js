9037efae6d29727cd5c89a6e5a380c6aec421e07
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-04 21:25:39 -0400

    refactor if statement to tenerary

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,5 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
-  } else {
-    console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
+  actual === expected ? console.log(`✅ Assertion Passed:  ${actual} === ${expected}`) : console.log(`😓 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 module.exports = assertEqual;
